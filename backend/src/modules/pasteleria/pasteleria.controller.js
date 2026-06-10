5166d71d103201b22f709e0bca23e07da51e9107
Emmanuel Salinas Caballero <emmanuelsalinascaballero2004@gmail.com>
2026-06-09 23:10:52 -0600

    Probando 22

--- backend/src/modules/pasteleria/pasteleria.controller.js
+++ backend/src/modules/pasteleria/pasteleria.controller.js
@@ -272,7 +272,7 @@ export const sharePedidoTicket = async (req, res) => {
     <head>
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <title>Comprobante de Pedido - 𝓛𝔂𝓪</title>
+      <title>Comprobante de pedido - 𝓛𝔂𝓪</title>
       <script src="https://cdn.tailwindcss.com"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js"></script>
